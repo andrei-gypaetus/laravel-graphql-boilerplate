8795e41b58c8a5d9375db826639c137154756184
Alex <alex@gypaetuscode.com>
2021-09-23 17:15:15 +0300

    Tests WIP

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,10 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\ParallelTesting;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\ClientRepository;
 
 trait CreatesApplication
 {
@@ -13,10 +17,29 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+        $app['config']->set('database.default', 'testbench');
+
         return $app;
+    }
+
+    /**
+     * Create a passport client for testing.
+     */
+    public function createClient()
+    {
+        $this->artisan('migrate', ['--database' => 'testbench']);
+        $client = app(ClientRepository::class)->createPasswordGrantClient(null, 'test', 'http://localhost');
+        config()->set('lighthouse-graphql-passport.client_id', $client->id);
+        config()->set('lighthouse-graphql-passport.client_secret', $client->secret);
     }
 }
